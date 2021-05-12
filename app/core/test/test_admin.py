3fc0f227f5cfe31eeabe35d5f1524f19303ef11c
Sherwin S. Abion <Sherwin.Abion@CONVERGEICT.COM>
2021-05-12 15:40:00 +0800

    fix on codechecker

--- app/core/test/test_admin.py
+++ app/core/test/test_admin.py
@@ -26,7 +26,7 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-    def test_user_change_page(self):
+    def test_user_page_change(self):
         """Test that user edit page works"""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
