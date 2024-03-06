c7a29808d834cd4c8626f014dd7a146d7e405dec
Sherwin S. Abion <Sherwin.Abion@CONVERGEICT.COM>
2024-03-06 15:41:44 +0800

    Database Configuration (fix linting 4)

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator Functions
 """
 
+
 def add(x, y):
     """ Add x and y and return results. """
     return x + y
+
 
 def subtract(x, y):
     """Subtract x and y and return results."""
